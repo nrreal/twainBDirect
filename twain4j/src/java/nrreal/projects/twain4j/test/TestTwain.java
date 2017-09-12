4c65fa59cf3afbe9c665fd89bada89d1706e6e12
theboshy <akumaokami10@gmail.com>
2017-09-12 11:43:25 -0500

    dll faltantes

--- twain4j/src/java/nrreal/projects/twain4j/test/TestTwain.java
+++ twain4j/src/java/nrreal/projects/twain4j/test/TestTwain.java
@@ -8,21 +8,23 @@ import org.omg.CORBA.SystemException;
  */
 public class TestTwain {
 
-    private static final String LIB_X32 = "nrreal/projects/twain4j/lib/x32";
-    private static final String LIB_X64 = "nrreal/projects/twain4j/lib/x64";
+    private static final String LIB_X32 = "nrreal/projects/twain4j/lib/x32/";
+    private static final String LIB_X64 = "nrreal/projects/twain4j/lib/x64/";
     private static final String ARCHITECTURE_X32 = "32";
+    private static final String NAME = "jtwain.dll";
 
     public static void main(String[] args) {
+        //--agregar dlls faltantes para utilizar jtwain
         String arch = System.getenv("PROCESSOR_ARCHITECTURE");
         String wow64Arch = System.getenv("PROCESSOR_ARCHITEW6432");
-        String realArch = arch.endsWith("32") || wow64Arch != null && wow64Arch.endsWith("32") ? "32" : "64";
-        if (System.getProperty("os.name").startsWith("windows")) {
+        String realArch = arch.endsWith(ARCHITECTURE_X32) || wow64Arch != null && wow64Arch.endsWith(ARCHITECTURE_X32) ? ARCHITECTURE_X32 : "64";
+        if (System.getProperty("os.name").startsWith("Windows")) {
             try {
                 if (realArch.equals(ARCHITECTURE_X32)) {
-                    System.load(LIB_X32);
+                    System.load(LIB_X32 + NAME);
                     System.out.println("cargada lib twain x32 ");
                 } else {
-                    System.load(LIB_X64);
+                    System.load(LIB_X64 + NAME);
                     System.out.println("cargada lib twain x64 ");
                 }
             } catch (SystemException e) {
