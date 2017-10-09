fef7928fcf7dd637cd3caaff7d46b93362ce78b5
theboshy <akumaokami10@gmail.com>
2017-10-09 14:03:43 -0500

    cambio de nombre para web twain

---
+++ twain4j/src/nrreal/projects/twain4j/lib/x64/twainInterfaceC.h
@@ -0,0 +1,16 @@
+
+
+#include "jni.h"
+#ifndef _Included_tilted_twain_JTwain
+#define _Included_tilted_twain_JTwain
+#ifdef __cplusplus
+extern "C" {
+#endif
+JNIEXPORT jboolean JNICALL _Java_tilted_twain_JTwain_isTwainAvailble(JNIEnv  *env, jobject obj);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
